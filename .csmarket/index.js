1f19e91e0dbd184c7192bd6196778eba0d5da11a
dannyrb <danny.ri.brown@gmail.com>
2018-12-23 14:28:43 -0500

    Add default images

--- .csmarket/index.js
+++ .csmarket/index.js
@@ -2,4 +2,12 @@ module.exports = {
     // Listing is complete, and should be shown in market
     shouldShow: true,
     name: "My Really Cool Plugin",
+    // IMAGE ASSETS
+    images: {
+        basePath: '.csmarket/img',
+        // 80x80 PNG or JPEG
+        featured: '/featured.png',
+        // 590x300 PNG or JPEG
+        listing: '/listing.png',
+    }
 }
