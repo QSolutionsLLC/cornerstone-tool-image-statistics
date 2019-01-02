9958a3e9b6229380bec0847177690712a317b9d7
dannyrb <danny.ri.brown@gmail.com>
2019-01-02 12:31:54 -0500

    ImageStatisticsTool implementation

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import cornerstone from 'cornerstone-core';
+import { import as csTools, toolColors } from 'cornerstone-tools';
+
+const BaseTool = csTools('base/BaseTool');
+const drawTextBox = csTools('drawing/drawTextBox');
+const getNewContext = csTools('drawing/getNewContext');
+
+export default class ImageStatistics extends BaseTool {
+  constructor(configuration = {}) {
+    const defaultConfig = {
+      name: 'ImageStatistics',
+      mixins: ['enabledOrDisabledBinaryTool']
+    };
+    const initialConfiguration = Object.assign(defaultConfig, configuration);
+
+    super(initialConfiguration);
+
+    this.initialConfiguration = initialConfiguration;
+  }
+
+  enabledCallback() {
+    this._forceImageUpdate();
+  }
+
+  disabledCallback() {
+    this._forceImageUpdate();
+  }
+
+  _forceImageUpdate() {
+    const enabledElement = cornerstone.getEnabledElement(this.element);
+    const hasImageToUpdate = enabledElement.image;
+
+    if (hasImageToUpdate) {
+      cornerstone.updateImage(this.element, true);
+    }
+  }
+
+  renderToolData(evt) {
+    const eventData = evt.detail;
+    const { canvasContext, image } = eventData;
+    const stats = image.stats;
+
+    const textLines = [];
+    const context = getNewContext(canvasContext.canvas);
+    const color = toolColors.getToolColor();
+
+    Object.keys(stats).forEach(function(key) {
+      const text = `${key} : ${stats[key]}`;
+
+      textLines.push(text);
+    });
+
+    drawTextBox(context, textLines, 0, 0, color);
+  }
+}
